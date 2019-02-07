e479c51b4c03c9b45f26a6c36fd85a0e5a6115cd
sqimiprojs <43413738+sqimiprojs@users.noreply.github.com>
2019-02-07 03:38:01 -0800

    Added and improved scaffolded item Categoria

---
+++ Candidaturas_BO/Controllers/CategoriasController.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Candidaturas_BO.Models;
+using OfficeOpenXml;
+
+namespace Candidaturas_BO.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private CandidaturasBOEntities db = new CandidaturasBOEntities();
+
+        // GET: Categorias
+        public ActionResult Index(string searchString, string sortOrder)
+        {
+            if (ADAuthorization.ADAuthenticate())
+            {
+                ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+                ViewBag.OrdemSortParm = sortOrder == "Ordem" ? "ordem_desc" : "Ordem";
+
+                List<Categoria> categorias = db.Categoria.ToList();
+
+                if (!String.IsNullOrEmpty(searchString))
+                {
+                    categorias = categorias.Where(s => s.Nome.Contains(searchString) || s.Nome.ToLower().Contains(searchString)).ToList();
+                }
+
+                //sort
+                switch (sortOrder)
+                {
+                    case "name_desc":
+                        categorias = categorias.OrderByDescending(s => s.Nome).ToList();
+                        break;
+                    case "Ordem":
+                        categorias = categorias.OrderBy(s => s.Ordem).ToList();
+                        break;
+                    case "ordem_desc":
+                        categorias = categorias.OrderByDescending(s => s.Ordem).ToList();
+                        break;
+                    default:
+                        categorias = categorias.OrderBy(s => s.Nome).ToList();
+                        break;
+                }
+
+                ViewBag.TotalCategorias = categorias.Count();
+
+                return View(categorias);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+        // GET: Categorias/Create
+        public ActionResult Create()
+        {
+            if (ADAuthorization.ADAuthenticate())
+            {
+                return View();
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+        // POST: Categorias/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Sigla,Nome,Ordem")] Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categoria.Add(categoria);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(categoria);
+        }
+
+        // GET: Categorias/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (ADAuthorization.ADAuthenticate())
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Categoria categoria = db.Categoria.Find(id);
+                if (categoria == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(categoria);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+        // POST: Categorias/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Sigla,Nome,Ordem")] Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(categoria).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(categoria);
+        }
+
+        // GET: Categorias/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (ADAuthorization.ADAuthenticate())
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Categoria categoria = db.Categoria.Find(id);
+                if (categoria == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(categoria);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+        // POST: Categorias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Categoria categoria = db.Categoria.Find(id);
+            db.Categoria.Remove(categoria);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET: Concelhos/MassInsert
+        public ActionResult MassInsert()
+        {
+            if (ADAuthorization.ADAuthenticate())
+            {
+                return View();
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult MassInsert(HttpPostedFileBase uploadFile)
+        {
+            if (uploadFile != null && uploadFile.ContentLength > 0)
+            {
+                if (ExcelValidator.HasExcelExtension(uploadFile) && ExcelValidator.HasExcelMimeType(uploadFile))
+                {
+                    string fileName = uploadFile.FileName;
+                    string fileContentType = uploadFile.ContentType;
+                    byte[] fileBytes = new byte[uploadFile.ContentLength];
+                    var data = uploadFile.InputStream.Read(fileBytes, 0, Convert.ToInt32(uploadFile.ContentLength));
+
+                    try
+                    {
+                        var package = new ExcelPackage(uploadFile.InputStream);
+                        var currentSheet = package.Workbook.Worksheets;
+                        var workSheet = currentSheet.First();
+                        var noOfCol = workSheet.Dimension.End.Column;
+                        var noOfRow = workSheet.Dimension.End.Row;
+
+                        for (int rowIterator = 2; rowIterator <= noOfRow; rowIterator++)
+                        {
+                            var nome = workSheet.Cells[rowIterator, 1].Value.ToString();
+                            var ordem = Convert.ToInt32(workSheet.Cells[rowIterator, 2].Value.ToString());
+                            var sigla = workSheet.Cells[rowIterator, 3].Value.ToString();
+
+                            if (!db.Categoria.Any(c => c.Nome == nome || c.Sigla == sigla || c.Ordem == ordem))
+                            {
+                                Categoria categoria = new Categoria
+                                {
+                                    Nome = nome,
+                                    Ordem = (short) ordem,
+                                    Sigla = sigla
+                                };
+
+                                db.Categoria.Add(categoria);
+                                db.SaveChanges();
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        ViewBag.ErrorMessage = "Tipo de ficheiro inválido. Por favor seleccione um ficheiro Excel válido.";
+                        return View();
+                    }
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Tipo de ficheiro inválido. Por favor seleccione um ficheiro Excel válido.";
+                    return View();
+                }
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Não foi seleccionado nenhum ficheiro. Por favor seleccione um ficheiro Excel válido.";
+                return View();
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
